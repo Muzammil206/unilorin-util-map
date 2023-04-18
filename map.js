fbce3eaf7bca67a20b82d8fcfd5f1d86706f9bad
Muzammil206 <ismailmuzammil206@gmail.com>
2023-04-18 15:53:48 +0100

    fixed

--- map.js
+++ map.js
@@ -1,63 +1,59 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibXV6YW1pbDIwNiIsImEiOiJjbGN5eXh2cW0wc2lnM290ZzJsZnNlbmxsIn0.o2Obvl7E_nQefSN34XsFmw';;
 
-
-// Define an array of map styles to switch between
-const styles = [
-  {
-    name: 'Streets',
-    id: 'mapbox://styles/mapbox/streets-v11'
-  },
-  {
-    name: 'Outdoors',
-    id: 'mapbox://styles/mapbox/outdoors-v11'
-  },
-  {
-    name: 'Satellite',
-    id: 'mapbox://styles/mapbox/satellite-v9'
-  },
-  {
-    name: 'Google Maps',
-    id: 'mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41' // Custom style that looks similar to Google Maps
-  }
-];
-
-// Create a new Mapbox map and set its initial configuration
+mapboxgl.accessToken = 'pk.eyJ1IjoibXV6YW1pbDIwNiIsImEiOiJjbGN5eXh2cW0wc2lnM290ZzJsZnNlbmxsIn0.o2Obvl7E_nQefSN34XsFmw';
 const map = new mapboxgl.Map({
   container: 'map',
-  style: styles[0].id, // Default style is Streets
+  style:  'mapbox://styles/mapbox/satellite-streets-v12',
   center: [ 4.675971, 8.484687], // nigeria coordinates
   zoom: 15 // Initial zoom level
 });
 
-// Add zoom and rotation controls to the map
+
 map.addControl(new mapboxgl.NavigationControl());
 
-// Create a custom style switcher control
-const styleSwitcher = {
-  onAdd: function(map) {
-    this._container = document.createElement('div');
-    this._container.className = 'mapboxgl-ctrl-group mapboxgl-ctrl';
-    styles.forEach((style) => {
-      const button = document.createElement('button');
-      button.className = 'mapboxgl-ctrl-icon';
-      button.title = style.name;
-      button.style.backgroundImage = `url(https://maps.tilehosting.com/styles/${style.id}.png?preview)`;
-      button.addEventListener('click', () => {
-        map.setStyle(style.id);
-      });
-      this._container.appendChild(button);
-    });
-    return this._container;
-  },
 
-  onRemove: function() {
-    this._container.parentNode.removeChild(this._container);
-    this._map = undefined;
+map.on('load', function()  {
+  map.addSource('states', {
+      'type': 'geojson',
+      // 'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson'
+      'data': 'test.geojson'
+      // 'data': 'https://services3.arcgis.com/BU6Aadhn6tbBEdyk/arcgis/rest/services/NGA_LGA_Boundaries_2/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+      
+
+  })
+  
+
+  
+
+  
+  map.addLayer({
+    'id': 'state-borders',
+    'type': 'line',
+    'source': 'states',
+    'layout': {},
+    'paint': {
+    'line-color': '#627BC1',
+    'line-width': 2
+}
+  });
+
+  
+  
+  
+
+})
+    
+
+
+const changeMap = function(){
+  const layerList = document.getElementById('menu');
+ const inputs = layerList.getElementsByTagName('input');
+  
+  for (const input of inputs) {
+  input.onclick = (layer) => {
+  const layerId = layer.target.id;
+  map.setStyle('mapbox://styles/mapbox/' + layerId);
+  };
+
   }
-};
-
-// Add the style switcher control to the map
-map.addControl(new mapboxgl.AttributionControl({
-  compact: true
-}), 'bottom-right');
-map.addControl(styleSwitcher, 'top-right');
+}
+changeMap()
